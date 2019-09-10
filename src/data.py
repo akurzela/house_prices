0f904477e7c8ad6149b44067d771e05242854857
Clement Roig <clemroig@amazon.com>
2019-09-10 18:54:26 +0200

    Create data loading pipeline

---
+++ src/data.py
@@ -0,0 +1,90 @@
+from os.path import exists, join
+from re import findall
+
+import pandas as pd
+
+from psycopg2 import connect
+
+from src.config import load_config
+
+
+def load_dataset(query: str):
+    """Load dataset from cache or database"""
+
+    path = join("data", "cache", f"{query}.csv")
+
+    if not exists(path):
+        df = fetch_dataset(query)
+        df.to_csv(path, sep="\t", index=False)
+
+    return pd.read_csv(path, sep="\t")
+
+
+def fetch_dataset(query: str):
+    """Fetch dataset from database using query in associated SQL file"""
+
+    credentials = _parse_credentials(query)
+    prepared_query = _prepare_query(query)
+
+    connection = connect(**credentials)
+    df = pd.io.sql.read_sql_query(prepared_query, connection)
+    connection.close()
+
+    return df
+
+
+# ---------------------------- Private functions ------------------------------
+# Build credentials dictionary based on key in header line in query SQL file
+def _parse_credentials(query):
+    # Query files should specify associated credentials on first line
+    with open(_get_query_path(query)) as f:
+        header = f.readline().strip()
+
+    config = load_config("credentials")
+    keys = {k for k, v in config.items() if type(v) is dict}
+
+    prefix = "-- Credentials: "
+    if not header.startswith(prefix):
+        msg = f'Query "{query}" missing header "{prefix}[{", ".join(keys)}]"'
+        raise QueryError(msg)
+
+    key = header[len(prefix) :]
+
+    if key not in keys:
+        raise QueryError(f'Credentials "{key}" not in "credentials.toml"')
+
+    credentials = {k: v for k, v in config.items() if type(v) is not dict}
+
+    return {**credentials, **config[key]}
+
+
+# Build SQL query, replacing parameters with values read from config file
+def _prepare_query(query):
+    with open(_get_query_path(query)) as f:
+        _ = f.readline()  # Skip credentials header line
+        query_sql = f.read()
+
+    # Extract query parameters from raw SQL
+    params = [p[1:-1] for p in findall(r"{[a-zA-Z_][a-zA-Z_\d]*}", query_sql)]
+
+    if not params:
+        return query_sql
+
+    args = load_config("queries")[query]
+
+    missing = list(set(params).difference(args.keys()))
+    if missing:
+        raise QueryError(f'Query "{query}" missing argument(s): {missing}')
+
+    unused = list(set(args.keys()).difference(params))
+    if unused:
+        raise QueryError(f'Unused argument(s) for query "{query}": {unused}')
+
+    return query_sql.format(**args)
+
+
+_get_query_path = lambda query: join("data", "queries", f"{query}.sql")
+
+
+class QueryError(Exception):
+    pass
