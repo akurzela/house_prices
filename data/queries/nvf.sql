e98c194f34602962912749190d0379056a082852
Clement Roig <clemroig@amazon.com>
2019-09-17 13:10:55 +0200

    Add query and config example

---
+++ data/queries/nvf.sql
@@ -0,0 +1,19 @@
+SELECT org, warehouse, metric_date, value
+FROM daily.d_sop_network_viewer_actuals
+WHERE
+  metric_name = 'New Vendor Freight'
+  AND current_flag = 1
+  AND metric_date between '{start}' and '{end}'
+  AND warehouse not in (
+    '3PL',
+    'Amazon Network',
+    'HeavyBulky',
+    'Mix',
+    'MixedSortability',
+    'Non Sortable',
+    'NonSortable',
+    'Others',
+    'Pantry',
+    'Sortable',
+    'Sortable DE'
+  )
