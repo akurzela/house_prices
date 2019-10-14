77ede26ae927f165b52ccee8b71675681fb683bb
ivallesp <ivanvallesperez@gmail.com>
2019-10-14 16:58:40 +0200

    Add log to the main module

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import logging.config
+
+from src.paths import get_log_config_filepath
+
+# Read config from file. This line must be run at all files being directly executed
+logging.config.fileConfig(get_log_config_filepath(), disable_existing_loggers=False)
+
+# This line must be added at the top level of each module
+logger = logging.getLogger(__name__)
+
+
 def main():
+    logger.info("This is a message example")
     pass
 
 
